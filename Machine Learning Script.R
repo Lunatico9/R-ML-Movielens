228b37a9cca2c9a0f4599797f017ab9e3f070bca
Lorenzo Luna <lorenzo.luna.def@hotmail.it>
2019-12-03 19:32:19 -0500

    Edited Report, added machine learning algorithm with RMSE = 0.86489

--- Machine Learning Script.R
+++ Machine Learning Script.R
@@ -1,6 +1,3 @@
-library(tidyverse)
-library(caret)
-
 ################################
 # Create edx set, validation set
 ################################
@@ -54,8 +51,48 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 #Machine Learning Algorithm
 ################################
 
+library(caret)
+library(tidyverse)
+
 #create training and test sets from edx
 
 train_ind = createDataPartition(edx$rating, p = 0.9, list = FALSE)
-train_edx = edx[trainind,]
-test_edx = edx[-trainind,]
+train_edx = edx[train_ind,]
+test_edx = edx[-train_ind,]
+
+#remove from test set users and movies not present in training set
+
+test_edx <- test_edx %>% 
+  semi_join(train_edx, by = "movieId") %>%
+  semi_join(train_edx, by = "userId")
+
+#define RMSE output function for solution evaluation
+
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
+#calculate overall rating mean
+mu <- mean(edx$rating)
+
+#calculate regularized movie bias with lambda = 3.25
+b_i <- edx %>% 
+  group_by(movieId) %>%
+  summarize(b_i = sum(rating - mu)/(n()+3.25))
+
+#calculate regularized user bias with lambda = 3.25
+b_u <- edx %>% 
+  left_join(b_i, by="movieId") %>%
+  group_by(userId) %>%
+  summarize(b_u = sum(rating - b_i - mu)/(n()+3.25))
+
+#predict ratings on the validation set
+predicted_ratings <- 
+  validation %>% 
+  left_join(b_i, by = "movieId") %>%
+  left_join(b_u, by = "userId") %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  pull(pred)
+
+#display resulting RMSE
+RMSE(predicted_ratings, validation$rating)
